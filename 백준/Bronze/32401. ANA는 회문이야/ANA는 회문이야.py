d984cd21fbbcc6c1ade1c7a5d35af2f18f038f2b
조우진 <69636525+SemiC0DER@users.noreply.github.com>
2025-05-12 16:31:00 +0900

    [Bronze II] Title: ANA는 회문이야, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/32401. ANA는 회문이야/ANA는 회문이야.py
+++ 백준/Bronze/32401. ANA는 회문이야/ANA는 회문이야.py
@@ -1,12 +1,13 @@
-#32401 ANA는 회문이야
-
 n = int(input())
 s = input()
 count = 0
-
+ana = []
 for i in range(n):
-    for j in range(i + 2, n):
-        if s[i] == 'A' and s[j] == 'A' and s[i : j+1].count('N') == 1 and s[i: j+1].count('A') == 2:
-            count += 1   
+    if s[i] == 'A':
+        ana.append(s[i])
+        if ana[-3:] == ['A', 'N', 'A']:
+            count += 1
+    elif s[i] == 'N':
+        ana.append(s[i])
 
 print(count)
