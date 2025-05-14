fd9d21fa2af8fda47d052083261b0f269e00d7cd
조우진 <69636525+SemiC0DER@users.noreply.github.com>
2025-05-14 18:28:18 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 412 ms, Memory: 258600 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,39 @@
+#1197 최소 스패닝 트리
+import sys
+sys.setrecursionlimit(100000)
+def getParent(vertex):
+    if parent[vertex] == vertex:
+        return vertex
+
+    parent[vertex] = getParent(parent[vertex])
+    return parent[vertex]
+
+def union(a, b):
+    a = getParent(a)
+    b = getParent(b)
+
+    if a == b:
+        return False
+
+    if a > b: a, b = b, a
+
+    parent[b] = a
+    return True
+
+v, e = map(int,  input().split())
+parent = list(range(v + 1))
+edges = []
+for _ in range(e):
+    a, b, c = map(int , input().split())
+    edges.append((c, a, b))
+
+edge_count = 0
+ans = 0
+
+for val, start, end in sorted(edges):
+    if union(start, end):
+        ans += val
+        edge_count += 1
+        if edge_count == v - 1:
+            print(ans)
+            break
